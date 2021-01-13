d73ae8166906c01dc93ed40278986ae43cfb879d
IvanRylkov <all0w3d@gmail.com>
2021-01-14 00:24:36 +0200

    update

---
+++ MyWebSite/assets/js/script.js
@@ -0,0 +1,13 @@
+// typing text
+
+document.addEventListener('DOMContentLoaded', function() {
+    let typed = new Typed('#typed', {
+        stringsElement: '#typed-strings', 
+        typeSpeed: 60,
+        backSpeed: 60,
+        loop: true
+
+      });
+      
+ }, false);
+
